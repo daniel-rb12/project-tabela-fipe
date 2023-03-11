69f98cd1283252906a7112d10f18b35af178d738
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2023-03-11 18:43:42 -0300

    feat: creating the login form and adding save-to-state logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
+  const [name, setName] = useState('');
+
   return (
-    <div>Home</div>
+    <form>
+      <label htmlFor="input-name">
+        Nome
+        <input
+          type="text"
+          name="input-name"
+          id="input-name"
+          value={ name }
+          onChange={ ({ target }) => setName(target.value) }
+        />
+      </label>
+      <button
+        type="button"
+      >
+        Enviar
+      </button>
+    </form>
   )
 }
 
