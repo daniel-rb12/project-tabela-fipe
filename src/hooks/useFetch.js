f83c1c933b0be771bd49fdf7c63a567af77e35b6
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2023-03-14 10:39:40 -0300

    feat: api request and brands options

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function useFetch(url) {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = (false);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   const requestAPI = async () => {
-    setIsLoading(true);
     try {
       const request = await fetch(url);
       const result = await request.json();
+      console.log(result)
       setData(result);
     } catch (error) {
       return error;
@@ -18,7 +18,12 @@ function useFetch(url) {
     }
   };
 
-  return { data, isLoading, requestAPI }
+  useEffect(() => {
+    requestAPI();
+  }, []);
+  
+
+  return { data, isLoading }
 }
 
 export default useFetch;
--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import useFetch from '../hooks/useFetch';
+import TypeContext from '../context/TypeContext';
 
-function Brands() {
+function Brands() {  
+  const { vehicle } = useContext(TypeContext);
+    
+  const url = `https://parallelum.com.br/fipe/api/v1/${vehicle}/marcas`;
+
+  const { data, isLoading } = useFetch(url);
+
+  if (isLoading) return <h1>Carregando...</h1>
   return (
-    <div>Brands</div>
+    <div>
+      <form>
+        <label htmlFor="brands">
+          Selecione:
+          <select
+            name="brands"
+            id="brands"
+          >
+            <option value="">Selecione uma opção...</option>
+            { data.map((option) => (
+              <option
+                key={ option.codigo }
+                value={ option.codigo }
+              >
+                { option.nome }
+              </option>
+            )) }
+          </select>
+        </label>
+      </form>
+    </div>
   )
 }
 
